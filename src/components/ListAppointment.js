7c7e2977ffcb5c40d497d378e23dd141fc8f6c8f
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-01-24 23:42:48 +0100

    Using moment and react-icons

--- src/components/ListAppointment.js
+++ src/components/ListAppointment.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { FaTimes } from 'react-icons/fa';
+import Moment from 'react-moment';
 
 class ListAppointment extends Component {
   render() {
@@ -8,13 +10,21 @@ class ListAppointment extends Component {
           ({ petName, aptDate, ownerName, aptNotes, aptId }) => (
             <div className='pet-item col media py-3' key={aptId}>
               <div className='mr-3'>
-                <button className='pet-delete btn btn-sm btn-danger'>X</button>
+                <button className='pet-delete btn btn-sm btn-danger'>
+                  <FaTimes />
+                </button>
               </div>
 
               <div className='pet-info media-body'>
                 <div className='pet-head d-flex'>
                   <span className='pet-name'>{petName}</span>
-                  <span className='apt-date ml-auto'>{aptDate}</span>
+                  <span className='apt-date ml-auto'>
+                    <Moment
+                      date={aptDate}
+                      parse="YYYY-MM-dd hh:mm"
+                      format="MMM-d h:mma"
+                     />
+                  </span>
                 </div>
 
                 <div className='owner-name'>
