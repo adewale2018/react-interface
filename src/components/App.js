dd302f007c9fdc27272ecf95b0a8b3a0f44ce36e
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-01-24 23:20:50 +0100

    Using a render template expression

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/App.css";
-import AddAppointments from './AddAppointment';
-import SearchAppointments from './SearchAppointment';
-import ListAppointments from './ListAppointment';
+import AddAppointments from "./AddAppointment";
+import SearchAppointments from "./SearchAppointment";
+import ListAppointments from "./ListAppointment";
 
-function App() {
-  return (
-    <main className='page bg-white' id='petratings'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-12 bg-white'>
-            <div className='container'>
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { myName: "Saheed", appointments: [], lastIndex: 0 };
+  }
+  componentDidMount() {
+    fetch("./data.json")
+      .then(res => res.json())
+      .then(result => {
+        const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1 });
+          return item;
+        });
+        this.setState({ appointments: apts });
+      });
+  }
+  render() {
+    return (
+      <main className='page bg-white' id='petratings'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-12 bg-white'>
+              <div className='container'>
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.appointments} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/components/ListAppointment.js
+++ src/components/ListAppointment.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ListAppointment extends Component {
   render() {
     return (
-      <div>
-        <h4>List-- Appointments</h4>
+      <div className='appointment-list item-list mb-3'>
+        {this.props.appointments.map(
+          ({ petName, aptDate, ownerName, aptNotes, aptId }) => (
+            <div className='pet-item col media py-3' key={aptId}>
+              <div className='mr-3'>
+                <button className='pet-delete btn btn-sm btn-danger'>X</button>
+              </div>
+
+              <div className='pet-info media-body'>
+                <div className='pet-head d-flex'>
+                  <span className='pet-name'>{petName}</span>
+                  <span className='apt-date ml-auto'>{aptDate}</span>
+                </div>
+
+                <div className='owner-name'>
+                  <span className='label-item'>Owner: </span>
+                  <span>{ownerName}</span>
+                </div>
+                <div className='apt-notes'>{aptNotes}</div>
+              </div>
+            </div>
+          )
+        )}
       </div>
     );
   }
 }
 
-export default  ListAppointment;
+export default ListAppointment;
